f1e94603bad0713e23f8aa59e2ee2e0dbd68c6c3
Marko Vikic <vikicmarko987@gmail.com>
2022-08-27 12:05:39 +0200

    TODO: Fix exposure.

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,13 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-#import os
+# import os
 
 from pathlib import Path
 
-#from dotenv import load_dotenv
-
-#load_dotenv()
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# TODO: Fix this exposure
 SECRET_KEY = 'django-insecure-zylj+o*4#3uht25g6y9%gi!wdzv2v$z52r58!k%7c%n(!n2wso'
 
 # SECURITY WARNING: don't run with debug turned on in production!
