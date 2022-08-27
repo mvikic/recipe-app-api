64bed03e86419facb203c838b65c3a7fbdf2f972
Marko Vikic <vikicmarko987@gmail.com>
2022-08-27 02:55:20 +0200

    Fixed Django secret key exposure.

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zylj+o*4#3uht25g6y9%gi!wdzv2v$z52r58!k%7c%n(!n2wso'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
